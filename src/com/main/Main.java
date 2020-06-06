39aac1a998ef4f736b08a880057818f7f447c32e
Zurab Benashvili <zura.benashvili@gmail.com>
2020-06-06 13:48:05 +0400

    feat: run socket server in main

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,18 @@
 package com.main;
 
+import com.logger.DefaultLogger;
+import com.logger.Logger;
+import com.socket.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("started!");
+        Logger logger = new DefaultLogger();
+        Server server = new Server(3002, logger);
+        try {
+            server.start();
+        } catch (Exception ex) {
+            System.out.println("Failed to start Socket Server");
+            ex.printStackTrace();
+        }
     }
 }
